7705eaea16021568305dfdeef67774d596fd90bd
Jackson <56370370+jtmoody01@users.noreply.github.com>
2020-10-21 11:37:43 -0600

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,10 +1,27 @@
+
 /**
- * class to capture relevant concept of a Car
  *
- * @author Tim Gesell
- * @version 08/18/2019
+ * @author Jackson, Andrew, Taro
+ * @version 10/21/20
  */
 public class Car
 {
+    String type;
+    String model;
+    String company;
+    int year;
+    double weight;
+    
+    public Car (String type, String model, String company, int year, double weight)
+    {
+        this.type = type;
+        this.model = model;
+        this.company = company;
+        this.year = year;
+        this.weight = weight;
+    }
 
+    public static void main(String args[])
+    {   
+    }
 }
